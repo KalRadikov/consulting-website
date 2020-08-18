e79ff77a3b338cb26eb0f41643ac04ce550d3ac2
Kal Radikov <14kr9@queensu.ca>
2020-08-17 22:19:48 -0600

    add kal headshot pt2

--- src/views/sections/AboutMe.vue
+++ src/views/sections/AboutMe.vue
@@ -27,7 +27,7 @@
         class="elevation-12 mb-12"
         size="128"
       >
-        <v-img src="https://cdn.vuetifyjs.com/images/john.png" />
+        <v-img :src="require('@/assets/kal-headshot.jpg')" />
       </v-avatar>
 
       <div />
