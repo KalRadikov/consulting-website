620a4b6953419894acf20e66ff22739e6c090dd5
Kal Radikov <14kr9@queensu.ca>
2020-08-17 20:44:24 -0600

    initial commit

---
+++ src/views/home/Index.vue
@@ -0,0 +1,38 @@
+
+<script>
+  // Extensions
+  import View from '@/views/View'
+
+  // Mixins
+  import LoadSections from '@/mixins/load-sections'
+
+  export default {
+    name: 'Home',
+
+    metaInfo: { title: 'Home' },
+
+    extends: View,
+
+    mixins: [
+      LoadSections([
+        'hero',
+        'about-me',
+        'features',
+        'theme-features',
+        'contact-us',
+        'affiliates',
+        'social-media',
+        'keep-in-touch',
+        'newsletter',
+        'info',
+      ]),
+    ],
+
+    props: {
+      id: {
+        type: String,
+        default: 'home',
+      },
+    },
+  }
+</script>
