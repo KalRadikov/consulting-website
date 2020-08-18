efbf31ca454369930dfdfe7eb4c2f019663e7ea7
Kal Radikov <14kr9@queensu.ca>
2020-08-17 22:31:14 -0600

    try fix 1

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/'
-    : '/',
+  publicPath: '/consulting-website/',
 }
