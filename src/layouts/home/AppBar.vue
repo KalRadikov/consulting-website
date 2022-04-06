4e046c579e940cce9275597cb24d397cba0ea9fd
kradikov <kaloyan.radikov@gettyimages.com>
2022-04-06 14:17:17 -0600

    fix tab spacing

--- src/layouts/home/AppBar.vue
+++ src/layouts/home/AppBar.vue
@@ -50,57 +50,47 @@
           >
             {{ name }}
           </v-tab>
-          <v-tabs
-            class="hidden-sm-and-down"
-            optional
-            hide-slider
-            active-class="text--primary"
-            min-width="96"
-          >
-            <v-tab :ripple="false">
-              <div class="text-center">
-                <v-tooltip
-                  v-if="!$vuetify.theme.dark"
-                  bottom
-                >
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      color="info"
-                      x-small
-                      fab
-                      v-on="on"
-                      @click="darkMode"
-                    >
-                      <v-icon class="mr-1">
-                        mdi-moon-waxing-crescent
-                      </v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Dark Mode On</span>
-                </v-tooltip>
+            <v-container class="d-flex align-center justify-center">
+              <v-tooltip
+                v-if="!$vuetify.theme.dark"
+                bottom
+              >
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    color="info"
+                    x-small
+                    fab
+                    v-on="on"
+                    @click="darkMode"
+                  >
+                    <v-icon class="mr-1">
+                      mdi-moon-waxing-crescent
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Dark Mode On</span>
+              </v-tooltip>
 
-                <v-tooltip
-                  v-else
-                  bottom
-                >
-                  <template v-slot:activator="{ on }">
-                    <v-btn
-                      color="info"
-                      x-small
-                      fab
-                      v-on="on"
-                      @click="darkMode"
-                    >
-                      <v-icon color="yellow">
-                        mdi-white-balance-sunny
-                      </v-icon>
-                    </v-btn>
-                  </template>
-                  <span>Dark Mode Off</span>
-                </v-tooltip>
-              </div>
-            </v-tab>
-          </v-tabs>
+              <v-tooltip
+                v-else
+                bottom
+              >
+                <template v-slot:activator="{ on }">
+                  <v-btn
+                    color="info"
+                    x-small
+                    fab
+                    v-on="on"
+                    @click="darkMode"
+                  >
+                    <v-icon color="yellow">
+                      mdi-white-balance-sunny
+                    </v-icon>
+                  </v-btn>
+                </template>
+                <span>Dark Mode Off</span>
+              </v-tooltip>
+            </v-container>
         </v-tabs>
       </div>
 
@@ -127,7 +117,7 @@
     data: () => ({
       drawer: null,
       items: [
-        // 'Home',
+        'Home',
         'Services',
         'Gallery',
         'About',
