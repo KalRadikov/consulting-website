b0058cffee3606ec80c27e92af99966d78cafbdb
kradikov <kaloyan.radikov@gettyimages.com>
2022-04-06 17:00:28 -0600

    make drawer menu dark/light theme

--- src/layouts/home/Drawer.vue
+++ src/layouts/home/Drawer.vue
@@ -1,32 +1,32 @@
 <template>
-  <v-navigation-drawer
-    bottom
-    color="transparent"
-    fixed
-    height="auto"
-    overlay-color="secondary"
-    overlay-opacity=".8"
-    temporary
-    v-bind="$attrs"
-    v-on="$listeners"
-  >
-    <v-list
-      color="white"
-      shaped
+  <v-theme-provider>
+    <v-navigation-drawer
+      bottom
+      fixed
+      height="auto"
+      overlay-color="secondary"
+      overlay-opacity=".8"
+      temporary
+      v-bind="$attrs"
+      v-on="$listeners"
     >
-      <v-list-item
-        v-for="name in items"
-        :key="name"
-        :to="{ name }"
-        :exact="name === 'Home'"
-        color="primary"
-      >
-        <v-list-item-content>
-          <v-list-item-title v-text="name" />
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-navigation-drawer>
+        <v-list
+          shaped
+        >
+          <v-list-item
+            v-for="name in items"
+            :key="name"
+            :to="{ name }"
+            :exact="name === 'Home'"
+            color="primary"
+          >
+            <v-list-item-content>
+              <v-list-item-title v-text="name" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  </v-theme-provider>
 </template>
 
 <script>
