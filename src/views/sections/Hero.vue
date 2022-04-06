14782c77b43627f3a091c04acd3b07a9388b3900
kradikov <kaloyan.radikov@gettyimages.com>
2022-04-06 11:28:00 -0600

    better wording front page

--- src/views/sections/Hero.vue
+++ src/views/sections/Hero.vue
@@ -22,16 +22,7 @@
                   :class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
                   class="font-weight-light"
                 >
-                  REALIZE YOUR ONLINE
-                </span>
-
-                <br>
-
-                <span
-                  :class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
-                  class="font-weight-black"
-                >
-                  POTENTIAL
+                  LET'S GET STARTED
                 </span>
               </v-col>
 
